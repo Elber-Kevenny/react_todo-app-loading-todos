97ac6bdad422a860b3da30b7244b47732a75d818
Elber-Kevenny <vieirabeb@gmail.com>
2026-03-14 21:02:47 -0300

    solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,30 @@
-import { Todo } from '../types/Todo';
+import { Patch, Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 4072;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
+};
+
+export const postCreateTodo = ({
+  title,
+  completed,
+  userId,
+}: Omit<Todo, 'id'>) => {
+  return client.post<Todo>('/todos', { title, completed, userId });
+};
+
+export const deleteTodo = (id: number) => {
+  return client.delete<Todo>(`/todos/${id}`);
+};
+
+export const updateTodo = ({ id, title }: Omit<Patch, 'completed'>) => {
+  return client.patch<Patch>(`/todos/${id}`, { id, title });
+};
+
+export const checkedTodo = ({ id, completed }: Omit<Patch, 'title'>) => {
+  return client.patch<Patch>(`/todos/${id}`, { id, completed });
 };
 
 // Add more methods here
--- src/types/Todo.ts
+++ src/types/Todo.ts
@@ -1,6 +1,24 @@
 export interface Todo {
-  id: number;
   userId: number;
   title: string;
   completed: boolean;
 }
+
+export interface Patch {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+export type TodoContextType = {
+  todo: Todo[];
+  handleSelected: (id: number) => void;
+  handleRemove: (id: number) => void;
+  handleFilterAll: () => void;
+  handleActive: () => void;
+  handleCompleted: () => void;
+  filteredTodo: Todo[];
+  filter: string;
+  handleRemoveCompleted: () => void;
+  setTodo: (todos: Todo[]) => void;
+};
